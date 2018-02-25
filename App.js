3c74df66029b15a5a3bc2745d43c971f49eb391c
Carlos Alberto Petit Quintero <ca.alberto.p@gmail.com>
2018-02-25 19:10:46 -0300

    Configuracion inicial, dependencias, pantallas de navegacion

--- App.js
+++ App.js
@@ -11,48 +11,71 @@ import {
   Text,
   View
 } from 'react-native';
+import {Container, Content} from 'native-base';
+import Swiper from 'react-native-swiper';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
 export default class App extends Component<Props> {
+    constructor() {
+        super()
+        this.state = {
+            outerScrollEnabled: true
+        }
+    }
+
+    verticalScroll = (index) => {
+        if (index !== 1) {
+            this.setState({
+                outerScrollEnabled: false
+            })
+        }
+        else {
+            this.setState({
+                outerScrollEnabled: true
+            })
+        }
+    }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <Container>
+        <Content>
+            <Swiper loop={false} showsPagination={false} index={1} scrollEnabled={this.state.outerScrollEnabled}>
+                <View style={styles.slideDefault}>
+                    <Text style={styles.text}>Chat</Text>
+                </View>
+                <Swiper loop={false} showsPagination={false} horizontal={false} index={1} onIndexChanged={(index) => this.verticalScroll(index)}>
+                <View style={styles.slideDefault}>
+                    <Text style={styles.text}>Buscar</Text>
+                </View>
+                <View style={styles.slideDefault}>
+                    <Text style={styles.text}>Camara</Text>
+                </View>
+                <View style={styles.slideDefault}>
+                    <Text style={styles.text}>Memorias</Text>
+                </View>
+            </Swiper>
+            <View style={styles.slideDefault}>
+              <Text style={styles.text}>Historias</Text>
+            </View>
+          </Swiper>
+        </Content>
+      </Container>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    slideDefault:{
+        flex: 1,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'#9DD6EB'
+    },
+    text:{
+        color:'white',
+        fontSize: 30,
+        fontWeight: 'bold'
+    }
 });
+
